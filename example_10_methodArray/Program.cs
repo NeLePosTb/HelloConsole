1ca09a9befd65a5eaf3fa5517e5814608f57a3bf
vlad <vmereskin@gmail.com>
2023-01-28 21:21:08 +0500

    add example 10

---
+++ example_10_methodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {21, 32, 43, 4, 18, 36, 18, 28};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }   
+    index++;
+}
